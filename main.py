0113aaf2cd40bf2022ebd6ea7ab5663548009b2b
Carlos Alexandre <alexandrec191@gmail.com>
2026-08-13 17:27:25 -0300

    Criando o arquivo do main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from telas.tela_principal import TelaPrincipal
+
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    janela = TelaPrincipal()
+    janela.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
