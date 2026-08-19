6d4d56f3330e63eeebeb3df0f155b79d8a4947dc
Carlos Alexandre <alexandrec191@gmail.com>
2026-08-19 11:18:13 -0300

    Aplicando o estilo

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from telas.tela_principal import TelaPrincipal
 def main():
 
     app = QApplication(sys.argv)
+    
+    app.setStyleSheet(ESTILO_GERAL)
 
     janela = TelaPrincipal()
     janela.show()
